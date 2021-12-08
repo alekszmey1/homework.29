6c735ed8900d083ac911adc39d3d987f95f8084a
alekszmey1 <86463231+alekszmey1@users.noreply.github.com>
2021-12-08 13:47:02 +0700

    третья   попытка

--- hw.29.1/main.go
+++ hw.29.1/main.go
@@ -7,35 +7,40 @@ import (
 
 func main()  {
 	var wg sync.WaitGroup
-	mu := new(sync.Mutex)
 	wg.Add(1)
 	fmt.Println("запускается функция main")
 	firstChan := make(chan int)
-	for i := 0; i <10 ; i++ {
-		go readOut(firstChan, &wg,i, mu)
-	}
-	for i := 0; i < 10; i++ {
-		go sqrt(firstChan,&wg, mu)
-	}
+	/*c := make(chan os.Signal, 1)
+	signal.Notify(c,syscall.SIGINT,syscall.SIGTERM)*/
+	go readOut(firstChan, &wg)
+	go sqrt(firstChan,/*c*/ &wg)
 	wg.Wait()
-
 	fmt.Println("the end")
 }
 
-func sqrt(f chan int, wg *sync.WaitGroup, mu *sync.Mutex){
-	wg.Add(1)
+func sqrt(f chan int,/*c chan os.Signal,*/ wg *sync.WaitGroup){
+	//wg.Add(1)
 	fmt.Println("запускается функция по возведению в квадрат")
 	defer func() {
 		wg.Done()
 	}()
 	 for x := range f {
-	 	mu.Unlock()
 	 	fmt.Println("запускается цикл по возведению в квадрат")
 	 	y := x*x
 		fmt.Println("квадрат равен", y)
+		 /*select {
+		 case <-f:
+		 	x := <-f
+		 	y:=x*x
+		 	fmt.Printf("квадрат %v равен %v\n",x,y)
+		 case <-c:
+		 	break
+		 default:
+		 	fmt.Println("выхожу из программы")*/
+		 }
 	}
-}
-func readOut(downstream chan int, wg *sync.WaitGroup, i int, mu *sync.Mutex) {
+//}
+func readOut(downstream chan int, wg *sync.WaitGroup) {
 	wg.Add(1)
 	fmt.Println("запускается функция чтение")
 	defer func() {
@@ -46,10 +51,10 @@ func readOut(downstream chan int, wg *sync.WaitGroup, i int, mu *sync.Mutex) {
 		fmt.Println("считыватель закрывает канал")
 		close(downstream)
 	}()
-	/*for i:= 0; i< 10000; i++ {
-		fmt.Println("запускается цикл по сканированию",i)*/
-		fmt.Printf("число %v положен в канал\n",i )
+	for i := 0; i < 1000000; i++ {
+		fmt.Println("запускается цикл по сканированию", i)
+			fmt.Printf("число %v положен в канал\n", i)
 		downstream <- i
-		mu.Lock()
 	}
+}
 
